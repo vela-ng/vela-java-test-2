fb74cf443a2a6b1ba9c457b537c257279b518acc
phayo <anazodogodwin@gmail.com>
2019-12-19 15:23:17 +0100

    Add configurations

---
+++ src/main/java/com/unbabel/challenge/configuration/ApiKey.java
@@ -0,0 +1,14 @@
+package com.unbabel.challenge.configuration;
+
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Service;
+
+@Data
+@Service
+@ConfigurationProperties(prefix = "unbabel")
+public class ApiKey {
+    private String url;
+    private String apiKey;
+}
